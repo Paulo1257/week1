37e89585f594c39e030a09d9567dd9e1896a7c43
c3669217 <p.pacheconoronhadecarvalho2387@student.ac.uk>
2023-10-05 11:17:28 +0100

    hello age.py

---
+++ built in fuctions.py
@@ -0,0 +1,15 @@
+name = input("What's your name? ")
+print("Hello there", name)
+age = input("Enter your age ")
+print("in one year your age will be", age + 1)
+age = input("Enter your age ")
+age = int(age)
+print("in one year your will be", age + 1)
+
+
+
+
+
+
+
+
