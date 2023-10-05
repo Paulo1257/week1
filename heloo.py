2e69bafa7682e5123bbf79c805f51ee72beb39d6
c3669217 <p.pacheconoronhadecarvalho2387@student.ac.uk>
2023-10-05 10:04:16 +0100

    hello age.py

---
+++ heloo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    name = input('Hello, what is your name? ')
+
+    year_now = int(input('What year is it today? '))
+    year_born = int(input('And what year were you born? '))
+
+    age = year_now - year_born
+
+    print(f'Hello, {name}. It is good to meet you. This year you will be {age} years old.')
